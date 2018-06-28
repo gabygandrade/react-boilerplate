e844b40e0fcedfaa92954a02bffc016a50dfcebd
Gaby Andrade <gaby_andrade@intuit.com>
2018-06-28 16:16:35 -0700

    Add axios, enzyme, react-test-renderer, react-material-icons, react-material, react-materilize

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,13 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  window.fetch = jest.fn().mockImplementation(() => ({
+    ok: true,
+    json: () => new Promise(resolve => {
+      resolve({})
+    }),
+  }))
+
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
