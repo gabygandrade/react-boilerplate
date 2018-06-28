a45ce4a2e33d1f88f87c41ff36e97af49535dcd2
Gaby Andrade <gaby_andrade@intuit.com>
2018-06-27 22:28:55 -0700

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {}
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(`https://dog.ceo/api/breed/bulldog/french/images`);
+      console.log({ response });
+      // do something with response
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   render() {
     return (
       <div className="App">
